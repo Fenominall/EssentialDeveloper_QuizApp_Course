fc102ddb0b0b5e821471130e687309c6bea6c575
Vladislav Todorov <it.vtodorov@gmail.com>
2022-08-18 16:53:40 +0300

    Introduce the new QuizDelegate protocol(it will replace the Router protocol)

--- QuizEngine/Router.swift
+++ QuizEngine/Router.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+public protocol QuizDelegate {
+    // Adding associatedtype for more geneirc flow, the questions and answers can be not only Strings, but images, videos etc. other types.
+    associatedtype Question: Hashable
+    associatedtype Answer
+    
+    func handle(question: Question, answerCallback: @escaping (Answer) -> Void)
+    func handle(result: Results<Question, Answer>)
+}
+
 // Depricating for changes to let the client migrate with no issues
 @available(*, deprecated)
 public protocol Router {
