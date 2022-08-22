271368ffc54a1f24267767c47616f5fdeea41ec2
Vladislav Todorov <it.vtodorov@gmail.com>
2022-08-22 12:00:27 +0300

    Renamed tests to clarify new intent

--- QuizEngineTests/QuizTests.swift
+++ QuizEngineTests/QuizTests.swift
@@ -9,54 +9,54 @@ import XCTest
 import QuizEngine
 
 class QuizTests: XCTestCase {
-    private var router: DeprecatedRouterSpy!
-    private var game: Game<String, String, DeprecatedRouterSpy>!
+    private var delegate: DelegateSpy!
+    private var quiz: Game<String, String, DelegateSpy>!
 
     override func setUp() {
         super.setUp()
-        router = DeprecatedRouterSpy()
-        game = startGame(
+        delegate = DelegateSpy()
+        quiz = startGame(
             questions: ["Q1", "Q2"],
-            router: router,
+            router: delegate,
             correctAnswers: ["Q1": "A1", "Q2": "A2"])
     }
     
     override func tearDown() {
-        router = nil
-        game = nil
+        delegate = nil
+        quiz = nil
         super.tearDown()
     }
     
-    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
-        router.anserCallback("wrong")
-        router.anserCallback("wrong")
+    func test_startQuiz_answerZeroOutOfTwoCorrectly_scoresZero() {
+        delegate.anserCallback("wrong")
+        delegate.anserCallback("wrong")
         
-        XCTAssertEqual(router.routedResult?.score, 0)
+        XCTAssertEqual(delegate.handledResult?.score, 0)
     }
     
-    func test_startGame_answerOneOutOfTwoCorrectly_scoresOne() {
-        router.anserCallback("A1")
-        router.anserCallback("wrong")
+    func test_startQuiz_answerOneOutOfTwoCorrectly_scoresOne() {
+        delegate.anserCallback("A1")
+        delegate.anserCallback("wrong")
         
-        XCTAssertEqual(router.routedResult?.score, 1)
+        XCTAssertEqual(delegate.handledResult?.score, 1)
     }
     
-    func test_startGame_answerTwoOutOfTwoCorrectly_scoresTwo() {
-        router.anserCallback("A1")
-        router.anserCallback("A2")
+    func test_startQuiz_answerTwoOutOfTwoCorrectly_scoresTwo() {
+        delegate.anserCallback("A1")
+        delegate.anserCallback("A2")
         
-        XCTAssertEqual(router.routedResult?.score, 2)
+        XCTAssertEqual(delegate.handledResult?.score, 2)
     }
     
-    private class DeprecatedRouterSpy: Router {
-        var routedResult: Results<String, String>? = nil
+    private class DelegateSpy: Router {
+        var handledResult: Results<String, String>? = nil
         var anserCallback: (String) -> Void = { _ in }
         
         func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
             self.anserCallback = answerCallback
         }
         func routeTo(result: Results<String, String>) {
-            routedResult = result
+            handledResult = result
         }
     }
 
