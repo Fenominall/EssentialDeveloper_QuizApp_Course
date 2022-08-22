54e84ab2357c8d694f21d4b5795b18b023579aad
Vladislav Todorov <it.vtodorov@gmail.com>
2022-08-22 11:39:24 +0300

    Move Router protocol to deprecated conformance file

--- QuizEngine/DeprecatedGame.swift
+++ QuizEngine/DeprecatedGame.swift
@@ -5,6 +5,17 @@
 //  Created by Fenominall on 7/22/22.
 //
 
+// Depricating for changes to let the client migrate with no issues
+@available(*, deprecated)
+public protocol Router {
+    // Adding associatedtype for more geneirc flow, the questions and answers can be not only Strings, but images, videos etc. other types.
+    associatedtype Question: Hashable
+    associatedtype Answer: Equatable
+    
+    func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
+    func routeTo(result: Results<Question, Answer>)
+}
+
 import Foundation
 
 @available(*, deprecated)
--- QuizEngine/QuizDelegate.swift
+++ QuizEngine/QuizDelegate.swift
@@ -15,14 +15,3 @@ public protocol QuizDelegate {
     func handle(question: Question, answerCallback: @escaping (Answer) -> Void)
     func handle(result: Results<Question, Answer>)
 }
-
-// Depricating for changes to let the client migrate with no issues
-@available(*, deprecated)
-public protocol Router {
-    // Adding associatedtype for more geneirc flow, the questions and answers can be not only Strings, but images, videos etc. other types.
-    associatedtype Question: Hashable
-    associatedtype Answer: Equatable
-    
-    func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
-    func routeTo(result: Results<Question, Answer>)
-}
