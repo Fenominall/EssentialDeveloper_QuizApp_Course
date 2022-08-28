a4e875bf743cb5777ced2a5ccfea1fd8299e885e
Vladislav Todorov <it.vtodorov@gmail.com>
2022-08-28 13:54:50 +0300

    Updated QuizTests to use new API

--- QuizEngineTests/QuizTests.swift
+++ QuizEngineTests/QuizTests.swift
@@ -9,55 +9,44 @@ import XCTest
 import QuizEngine
 
 class QuizTests: XCTestCase {
-    private var delegate: DelegateSpy!
     private var quiz: Quiz!
     
-    override func setUp() {
-        super.setUp()
-        delegate = DelegateSpy()
+    func test_startQuiz_answerZeroOutOfTwoCorrectly_scoresZero() {
+        let delegate = DelegateSpy()
         quiz = Quiz.start(
             questions: ["Q1", "Q2"],
             delegate: delegate,
             correctAnswers: ["Q1": "A1", "Q2": "A2"])
-    }
-    
-    override func tearDown() {
-        delegate = nil
-        quiz = nil
-        super.tearDown()
-    }
-    
-    func test_startQuiz_answerZeroOutOfTwoCorrectly_scoresZero() {
-        delegate.answerCompletion("wrong")
-        delegate.answerCompletion("wrong")
         
-        XCTAssertEqual(delegate.handledResult?.score, 0)
-    }
-    
-    func test_startQuiz_answerOneOutOfTwoCorrectly_scoresOne() {
-        delegate.answerCompletion("A1")
-        delegate.answerCompletion("wrong")
-        
-        XCTAssertEqual(delegate.handledResult?.score, 1)
-    }
-    
-    func test_startQuiz_answerTwoOutOfTwoCorrectly_scoresTwo() {
         delegate.answerCompletion("A1")
         delegate.answerCompletion("A2")
         
-        XCTAssertEqual(delegate.handledResult?.score, 2)
+        XCTAssertEqual(delegate.completedQuizzes.count, 1)
+        assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
+    }
+
+    // MARK: - Helpers
+    private func assertEqual(_ a1: [(String, String)],
+                             _ a2: [(String, String)],
+                             file: StaticString = #filePath,
+                             line: UInt = #line) {
+        XCTAssertTrue(a1.elementsEqual(a2, by: ==), "\(a1) is not equal to \(a2)",
+                      file: file, line: line)
     }
     
     private class DelegateSpy: QuizDelegate {
-        var handledResult: Results<String, String>? = nil
+        var completedQuizzes: [[(String, String)]] = []
         var answerCompletion: (String) -> Void = { _ in }
         
-        func answer(for: String, completion: @escaping (String) -> Void) {
+        func answer(for question: String, completion: @escaping (String) -> Void) {
             self.answerCompletion = completion
         }
         
-        func handle(result: Results<String, String>) {
-            handledResult = result
+        func didCompleteQuiz(withAnswers answers: [(question: String, answer: String)]) {
+            completedQuizzes.append(answers
+            )
         }
+        
+        func handle(result: Results<String, String>) {}
     }
 }
