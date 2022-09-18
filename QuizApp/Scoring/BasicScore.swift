8e9d5e9965bf0ef65cb121840e03e620563b472a
Vladislav Todorov <it.vtodorov@gmail.com>
2022-09-18 16:24:12 +0300

    made score method generic to avoid error with types conversion

--- QuizApp/Scoring/BasicScore.swift
+++ QuizApp/Scoring/BasicScore.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public final class BasicScore {
-    public static func score(for answers: [String], comparingTo correctAnswers: [String]) -> Int {
+    public static func score<T: Equatable>(for answers: [T], comparingTo correctAnswers: [T]) -> Int {
         return zip(answers, correctAnswers).reduce(0) { score, tuple in
             return score + (tuple.0 == tuple.1 ? 1 : 0)
         }
