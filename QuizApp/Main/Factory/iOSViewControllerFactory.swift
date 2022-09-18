1b470e757bb982bbe0ff2c243dc1b96e55727dca
Vladislav Todorov <it.vtodorov@gmail.com>
2022-09-18 12:58:58 +0300

    Removed deprecated initializer

--- QuizApp/Main/Factory/iOSViewControllerFactory.swift
+++ QuizApp/Main/Factory/iOSViewControllerFactory.swift
@@ -9,32 +9,28 @@ import QuizEngine
 import UIKit
 
 class iOSViewControllerFactory: ViewControllerFactory {
+    // MARK: - Properties
     typealias Answers = [(question: Question<String>, answers: [String])]
     
     private var questions = [Question<String>]()
     private var options = [Question<String>: [String]]()
     private let correctAnswers: () -> Answers
     
+    // MARK: - Initializers
     init(options: [Question<String>: [String]], correctAnswers: Answers) {
         self.questions = correctAnswers.map { $0.question }
         self.correctAnswers = { correctAnswers }
     }
-    
-    private init(questions: [Question<String>], options: [Question<String>: [String]], correctAnswers: [Question<String>: [String]]) {
-        self.questions = questions
-        self.options = options
-        self.correctAnswers = { questions.map { question in
-            (question, correctAnswers[question]!)}
-        }
-    }
-    
+
+    // MARK: - Methods
     func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController {
         guard let options = self.options[question] else {
             fatalError("Couldnot find options for question")
         }
         return questionViewController(for: question, options: options, answerCallback: answerCallback)
     }
-    
+
+    // MARK: - Helpers
     private func questionViewController(
         for question: Question<String>,
         options: [String],
@@ -48,7 +44,7 @@ class iOSViewControllerFactory: ViewControllerFactory {
             }
         }
     
-    // MARK: - Helpers
+    
     private func questionViewController(
         for question: Question<String>,
         value: String,
