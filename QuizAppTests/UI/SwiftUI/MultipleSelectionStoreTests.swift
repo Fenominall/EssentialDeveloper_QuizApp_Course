39e545033eca83477a933a2951b54b1c244fef5b
Vladislav Todorov <it.vtodorov@gmail.com>
2022-09-23 13:23:39 +0300

    toggle selection state

---
+++ QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -0,0 +1,41 @@
+//
+//  MultipleSelectionStoreTests.swift
+//  QuizAppTests
+//
+//  Created by Fenominall on 9/23/22.
+//
+
+import XCTest
+@testable import QuizApp
+
+struct MultipleSelectionStore {
+    var options: [MultipleSelectionOption]
+    
+    init(options: [String]) {
+        self.options = options.map({ MultipleSelectionOption(text: $0) })
+    }
+}
+
+struct MultipleSelectionOption {
+    let text: String
+    var isSelected = false
+    
+    mutating func select() {
+        isSelected.toggle()
+    }
+}
+ 
+final class MultipleSelectionStoreTests: XCTestCase {
+    
+    func test_() {
+        var sut = MultipleSelectionStore(options: ["o0, o1"])
+        
+        // mutating an option within an array
+        // when mutated a new value gets assigned to the SUT
+        sut.options[0].select()
+        XCTAssertTrue(sut.options[0].isSelected)
+        
+        sut.options[0].select()
+        XCTAssertFalse(sut.options[0].isSelected)
+    }
+}
