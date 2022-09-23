81c11493fdebe9f22f237da38ff829de388dba8d
Vladislav Todorov <it.vtodorov@gmail.com>
2022-09-23 13:47:43 +0300

    Move  MultipleSelection store to prod target

--- QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
+++ QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -7,35 +7,6 @@
 
 import XCTest
 @testable import QuizApp
-
-struct MultipleSelectionStore {
-    var options: [MultipleSelectionOption]
-    var canSubmit: Bool {
-        // Cheking if an option was selected to toggle the state of canSubmit
-        // The array of options shouldnot be empty
-        !options.filter(\.isSelected).isEmpty
-    }
-    private let handler: ([String]) -> Void
-    
-    init(options: [String], handler: @escaping ([String]) -> Void = { _ in }) {
-        self.options = options.map({ MultipleSelectionOption(text: $0) })
-        self.handler = handler
-    }
-    
-    func submit() {
-        guard canSubmit else { return }
-        handler(options.filter(\.isSelected).map(\.text))
-    }
-}
-
-struct MultipleSelectionOption {
-    let text: String
-    var isSelected = false
-    
-    mutating func select() {
-        isSelected.toggle()
-    }
-}
  
 final class MultipleSelectionStoreTests: XCTestCase {
     
